4cd4de93d448483d0f1f32303c4e8bd414477a50
readone <android.armv7a@gmail.com>
2015-02-18 07:34:31 +0700

    wifi: Allow building without Wifi HAL
     * For example devices with older broadcom chips with driver built
       as module doesn't like this. When the device goes into suspend
       mode, the wifi service crashes and the device soft reboots.
    
     * To not build the HAL, use:
       BOARD_NO_WIFI_HAL := true
    
    Change-Id: Ia186ce3a59a082cfe98d283492fa450014af52f4

--- service/Android.mk
+++ service/Android.mk
@@ -42,16 +42,16 @@ include $(BUILD_STATIC_LIBRARY)
 # ============================================================
 LIB_WIFI_HAL := libwifi-hal
 
-ifneq ($(BOARD_NO_WIFI_HAL), true)
-  ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
-    LIB_WIFI_HAL := libwifi-hal-bcm
-  else ifeq ($(BOARD_WLAN_DEVICE), qcwcn)
-    LIB_WIFI_HAL := libwifi-hal-qcom
-  else ifeq ($(BOARD_WLAN_DEVICE), mrvl)
-    # this is commented because none of the nexus devices
-    # that sport Marvell's wifi have support for HAL
-    # LIB_WIFI_HAL := libwifi-hal-mrvl
-  endif
+ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
+  LIB_WIFI_HAL := libwifi-hal-bcm
+else ifeq ($(BOARD_WLAN_DEVICE), qcwcn)
+  # this is commented because none of the nexus devices
+  # that sport Qualcomm's wifi have support for HAL
+  # LIB_WIFI_HAL := libwifi-hal-qcom
+else ifeq ($(BOARD_WLAN_DEVICE), mrvl)
+  # this is commented because none of the nexus devices
+  # that sport Marvell's wifi have support for HAL
+  # LIB_WIFI_HAL := libwifi-hal-mrvl
 endif
 
 # Build the HalUtil
